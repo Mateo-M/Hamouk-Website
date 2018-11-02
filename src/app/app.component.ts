c8d35216314327a88e9502088dc1bfdcbd1b8db3
Mateo-M <me.mateomeana@gmail.com>
2018-11-02 12:43:32 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hambouk';
+
+export class AppComponent implements OnInit {
+  title: 'Hambouk';
+  constructor(private spinner: NgxSpinnerService) { }
+  ngOnInit() {
+    /** spinner starts on init */
+    this.spinner.show();
+    setTimeout(() => {
+        /** spinner ends after 2 seconds */
+        this.spinner.hide();
+    }, 2500);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StatusComponent } from './status/status.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Accueil' }
+  },
+  {
+    path: 'status',
+    component: StatusComponent,
+    data: { title: 'Status' }
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StatusComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    HttpClientModule,
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
