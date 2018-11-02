ad94b8aef93bf8aa8c4f605c45fc787f049f74e6
Mateo-M <me.mateomeana@gmail.com>
2018-11-02 12:46:41 +0100

    add status

---
+++ src/app/status/status.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ServersService } from '../services/servers.service';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.component.html',
+  styleUrls: ['./status.component.css']
+})
+export class StatusComponent implements OnInit {
+
+  constructor(private serversService: ServersService) { }
+
+  ngOnInit() {
+    console.log(this.serversService.getServersInfo());
+  }
+}
