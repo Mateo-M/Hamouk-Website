28e5ddd2adb3346ce1a50252df14ffaa20f87919
Mateo-M <me.mateomeana@gmail.com>
2019-03-16 15:50:38 +0100

    Add SSL to Heroku deployed app

--- server.js
+++ server.js
@@ -14,3 +14,18 @@ app.get('/*', function (req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// Heroku automagically gives us SSL
+// Lets write some middleware to redirect us
+let env = process.env.NODE_ENV || 'development';
+
+let forceSSL = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+if (env === 'production') {
+  app.use(forceSSL);
+}
