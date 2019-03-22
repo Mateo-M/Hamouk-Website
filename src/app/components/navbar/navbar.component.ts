b785a610f1706bfc363f93e3eefce6c640288508
Mateo-M <me.mateomeana@gmail.com>
2019-03-22 11:51:15 +0100

    Added login button & User to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { take, map, tap, filter } from 'rxjs/operators';
+
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  public show = false;
+  constructor(public auth: AuthService, public router: Router) { }
 
   ngOnInit() {
+    this.auth.user$.pipe(
+      map(user => !!user),
+      filter((loggedIn) => loggedIn),
+      take(1),
+      tap(loggedIn => {
+        if (loggedIn) {
+          console.log('Already logged in');
+          this.router.navigate(['/']);
+        }
+      })
+    ).subscribe();
   }
 
 }
