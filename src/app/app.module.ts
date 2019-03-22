302072e698c055ba865e261bd44923da6cd286bf
Mateo-M <me.mateomeana@gmail.com>
2019-03-22 11:48:28 +0100

    Added AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { StatusComponent } from './status/status.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { StaffComponent } from './staff/staff.component';
+import { AdminComponent } from './admin/admin.component';
 
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthGuard } from './services/auth.guard';
+import { AuthService } from './services/auth.service';
+
+import { environment } from '../environments/environment';
+
+const config = {
+  apiKey: 'AIzaSyDa5HuDzQ8e5GMaimCOjIUHG1pFt_8Wi8c',
+  authDomain: 'hamouk-website.firebaseapp.com',
+  databaseURL: 'https://hamouk-website.firebaseio.com',
+  projectId: 'hamouk-website',
+  storageBucket: 'hamouk-website.appspot.com',
+  messagingSenderId: '95458672379'
+};
 
 const appRoutes: Routes = [
   {
@@ -26,16 +46,27 @@ const appRoutes: Routes = [
     path: 'about',
     component: AboutComponent
   },
+  {
+    path: 'staff',
+    component: StaffComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AuthGuard]
+  },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    FooterComponent,
     HomeComponent,
     StatusComponent,
     AboutComponent,
-    NavbarComponent,
-    FooterComponent,
+    StaffComponent,
+    AdminComponent,
   ],
   imports: [
     NgbModule,
@@ -46,8 +77,15 @@ const appRoutes: Routes = [
     ),
     HttpClientModule,
     NgxSpinnerModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AuthGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
