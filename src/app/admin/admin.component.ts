0409d987623684bc92a0385ea1fb9bbc51fa718e
Mateo-M <me.mateomeana@gmail.com>
2019-03-24 11:28:30 +0100

    Added username input w/ validators

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { StaffService } from '../services/staff.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+  username = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(16),
+    Validators.pattern('^[a-zA-Z0-9_]+$')
+  ]);
 
-  constructor() { }
+  constructor(private staffService: StaffService) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.staffService
+      .getUsernameUuid(this.username.value)
+      .subscribe());
+  }
 }
