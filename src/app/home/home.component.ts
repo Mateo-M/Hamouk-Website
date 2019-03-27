64c4bacaddbbdadbfbdd75093dc88e0392308272
Mateo-M <me.mateomeana@gmail.com>
2019-03-27 17:01:07 +0100

    Added launcher

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,13 +7,9 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private modalService: NgbModal) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  openSm(content) {
-    this.modalService.open(content, { centered: true });
   }
 
 }
